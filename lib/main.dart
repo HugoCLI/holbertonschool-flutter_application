8d7a95714433dbad0ef5ee73cce922bde3cebbdd
= <hugo.chilemme@gmail.com>
2022-12-09 14:11:47 +0100

    Create Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter/src/painting/edge_insets.dart';
+import 'package:flutter/src/painting/box_decoration.dart';
+import 'dart:ui';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class MyApp extends StatefulWidget  {
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginPage(),
-    );
-  }
+  LoginPage createState() => LoginPage();
 }
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
 
+class LoginPage extends State<MyApp> {
+  bool showNewWidget = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.network(
-              'https://images.squarespace-cdn.com/content/v1/60bf70d860f31b4f60455443/7e02aa09-0395-4fa0-8639-67c3cb77f062/Holberton+School+Logo+white_fog.png',
-              height: 100,
-            ),
-            const SizedBox(height: 16),
-            TextButton(
-              child: const Text(
-                'Connect',
-                style: TextStyle(color: Colors.white),
+        backgroundColor: const Color(0xFF121212),
+        resizeToAvoidBottomInset: false,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.network(
+                'https://images.squarespace-cdn.com/content/v1/60bf70d860f31b4f60455443/7e02aa09-0395-4fa0-8639-67c3cb77f062/Holberton+School+Logo+white_fog.png',
+                height: 50,
               ),
-              buttonStyle: const ButtonStyle(
-                backgroundColor: Color(0xFFEB0145),
+              const SizedBox(height: 16),
+              Container(
+                padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width * 0.2,
+                  right: MediaQuery.of(context).size.width * 0.2,
+                ),
+                margin: EdgeInsets.only(top: 150.0),
+                width: double.infinity,
+                child: TextButton.icon(
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color(0xFFEB0145), // Text Color
+                    padding: EdgeInsets.all(22.0),
+                  ),
+                  onPressed: () {
+                    // Action à effectuer lorsque le bouton est pressé
+                  },
+                  icon: const FaIcon(
+                    FontAwesomeIcons.google,
+                    size: 15.0,
+                    color: Colors.white,
+                  ),
+                  label: const Text(
+                    'Connect',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
               ),
-              onPressed: () {
-                // Action à effectuer lorsque le bouton est pressé
-              },
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-    );
+      );
   }
 }
