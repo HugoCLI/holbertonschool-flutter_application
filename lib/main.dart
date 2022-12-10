a85ca59509f428d801965a86a4c28077b7cb2baf
= <hugo.chilemme@gmail.com>
2022-12-10 16:08:01 +0100

    Adding Attendance

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_const
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -20,7 +22,23 @@ void main() {
   );
 }
 
+class GoogleUserAvatar extends StatelessWidget {
+  final String? photoUrl;
+  final double width;
 
+  const GoogleUserAvatar({super.key, 
+    this.photoUrl,
+    this.width = 50,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundImage: NetworkImage(photoUrl!),
+      radius: width / 2,
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -54,11 +72,8 @@ class LoginPage extends StatelessWidget {
               ),
               const SizedBox(height: 16),
               Container(
-                padding: EdgeInsets.only(
-                  left: MediaQuery.of(context).size.width * 0.2,
-                  right: MediaQuery.of(context).size.width * 0.2,
-                ),
-                margin: const EdgeInsets.only(top: 150.0),
+                padding:  EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.height * 0.1),
+                margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.1),
                 width: double.infinity,
                 child: TextButton.icon(
                   style: TextButton.styleFrom(
@@ -92,65 +107,118 @@ class LoginPage extends StatelessWidget {
 }
 
 
-// ignore: use_key_in_widget_constructors
+// ignore: use_key_in_widget_constructors, must_be_immutable
 class DashboardPage extends StatelessWidget {
+  static const String eventname = "Check-in";
+
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const SizedBox(height: 75),
+            Column(
+              mainAxisAlignment:  MainAxisAlignment.center,
+              children: [
+                const GoogleUserAvatar(
+                  photoUrl: "https://lh3.googleusercontent.com/a/AEdFTp4Is-DC6XS_CXTG7yUFZFFSZ3L5xS6yIxusDYgnnw=s96-c-rg-br100",
+                  width: 100,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.height * 0.1) + const EdgeInsets.only(top: 50),
+                  child: Column(
+                    children: [
+                      const Text("Welcome back"),
+                      const Text(
+                        "Hugo Chilemme",
+                        style: TextStyle(
+                            fontSize: 25.0,
+                            height: 2,
+                            fontWeight: FontWeight.w800 
+                        ),
+                      ),
+                      Container(
+
+                        margin: EdgeInsets.only(top: MediaQuery.of(context).size.width * 0.1),
+                        width: double.infinity,
+                        child: TextButton.icon(
+                          style: TextButton.styleFrom(
+                            backgroundColor: const Color(0xFFEB0145), // Text Color
+                            padding: const EdgeInsets.all(22.0),
+                          ),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>  const AttendancePage(event: eventname),
+                              ),
+                            );
+                          },
+                          icon: const Icon(
+                            Icons.access_time,
+                            size: 20.0,
+                            color: Colors.white,
+                          ),
+                          label: const Text(
+                            "Please check your $eventname",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+
+          ],
+        )
+      )
+    );
+  }
+}
+
+
+// ignore: use_key_in_widget_constructors, must_be_immutable
+class AttendancePage extends StatelessWidget {
+  final String event;
+  const AttendancePage({super.key, required this.event});
+
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Padding(
+        padding: const EdgeInsets.all(100.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:  [
             const Text(
-              "Check-in",
+              "Are you face-to-face or remote ?",
               style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.w800,
-              ),
-            ),
-            const SizedBox(height: 15),
-            const Text(
-              "Remaining 3 minutes",
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 250),
-            const CircleAvatar(
-              backgroundImage: NetworkImage(
-                "https://lh3.googleusercontent.com/a/AEdFTp4Is-DC6XS_CXTG7yUFZFFSZ3L5xS6yIxusDYgnnw=s96-c-rg-br100",
-              ),
-              radius: 50,
-            ),
-            const SizedBox(height: 50),
-            const Text(
-              "Good morning",
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              "Hugo Chilemme",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.w800,
+                fontSize: 25.0,
+                height: 2,
+                fontWeight: FontWeight.w600 
               ),
             ),
             Container(
-                padding: EdgeInsets.only(
-                  left: MediaQuery.of(context).size.width * 0.2,
-                  right: MediaQuery.of(context).size.width * 0.2,
-                ),
-                margin: const EdgeInsets.only(top: 150.0),
-                width: double.infinity,
-                child: TextButton.icon(
+
+              margin: const EdgeInsets.only(top: 50.0),
+              width: double.infinity,
+              child:  TextButton(
                   style: TextButton.styleFrom(
-                    backgroundColor: const Color(0xFF121212), // Text Color
+                    backgroundColor: const Color(0xFFEB0145), // Text Color
                     padding: const EdgeInsets.all(22.0),
                   ),
                   onPressed: () {
+                    // ignore: avoid_print
+                    print("face-to-face");
                     Navigator.push(
                       context,
                       MaterialPageRoute(
@@ -158,22 +226,40 @@ class DashboardPage extends StatelessWidget {
                       ),
                     );
                   },
-                  icon: const Icon(
-                    Icons.call_merge,
-                    size: 20.0,
-                    color: Colors.white,
-                  ),
-                  label: const Text(
-                    'Please check your attendance',
+                  child: const Text(
+                    "I'am in face to face",
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
-              ),
+            ),
+            Container(
+
+              margin: const EdgeInsets.only(top: 20.0),
+              width: double.infinity,
+              child:  TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: const Color(0xFFEEEEEE), // Text Color
+                    padding: const EdgeInsets.all(22.0),
+                  ),
+                  onPressed: () {
+                    // ignore: avoid_print
+                    print("remote");
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DashboardPage(),
+                      ),
+                    );
+                  },
+                  child: const Text(
+                    "I'am in remote",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+            ),
           ],
-        ),
-      ),
+        )
+      )
     );
   }
 }
-
-
